9bfbb56e1963ba8541d17eb8af2b84193945ed8f
alperen569 <alperencol412@gmail.com>
2026-07-19 18:28:12 +0300

    a

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.use(
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.tailwindcss.com"],
+        scriptSrcAttr: ["'unsafe-inline'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
         fontSrc: ["'self'", "https://fonts.gstatic.com", "data:"],
         imgSrc: ["'self'", "data:"],
