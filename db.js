a3cb91c110389f3f9f73a0c8eed332e3bec3e018
alperen569 <alperencol412@gmail.com>
2026-07-28 14:55:22 +0300

    tyqwe

--- db.js
+++ db.js
@@ -4,7 +4,16 @@ require("dotenv").config();
 
 
 let pool;
-
+pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 async function initializeDatabase() {
   await pool.query(`
 CREATE TABLE IF NOT EXISTS users (
